eef0b6293b7d7ecc538660bc5dd9cd177e16346c
Deus Agency <you@example.com>
2026-05-09 05:33:20 +0100

    fix logout

--- mon_asbl/urls.py
+++ mon_asbl/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from membres import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,9 @@ urlpatterns = [
 
     # 🔐 AUTH
     path('accounts/', include('django.contrib.auth.urls')),
+    path(
+    'accounts/logout/',
+    auth_views.LogoutView.as_view(),
+    name='logout'
+),
 ]
